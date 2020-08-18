93038cffd28890af8b7cdf95f36e28b85d2d8d3a
Matt <mw283@cam.ac.uk>
2020-08-18 02:43:24 +0100

    modified:   example/patch.xodp 	new file:   example2/patch.xodp 	new file:   get-data-rate/patch.cpp 	modified:   get-data-rate/patch.xodp 	new file:   get-device-id/patch.cpp 	modified:   get-device-id/patch.xodp 	modified:   lis3dh-device/patch.xodp 	new file:   read/patch.cpp 	modified:   read/patch.xodp

---
+++ get-data-rate/patch.cpp
@@ -0,0 +1,20 @@
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    // The node responds only if there is an input pulse
+    if (!isInputDirty<input_UPD>(ctx))
+        return;
+    
+    // Get a pointer to the `Adafruit_LIS3DH` class instance
+    auto sensor = getValue<input_DEV>(ctx);
+
+
+
+    emitValue<output_RATE>(ctx, sensor->getDataRate());
+    emitValue<output_DONE>(ctx, 1);
+            
+}
